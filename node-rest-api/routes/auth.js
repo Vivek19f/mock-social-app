ea72adb947ae10aa8174843d6be6726ddbc4ba5b
Vivek <vivek.jhooria@valuemomentum.com>
2021-10-15 19:10:15 +0530

    api completed UI- Homepage profile page completed

---
+++ node-rest-api/routes/auth.js
@@ -0,0 +1,43 @@
+const User = require("../models/User");
+
+const router = require("express").Router();
+
+const bcrypt = require("bcrypt");
+
+//Register
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = await new User({
+      username: req.body.user,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user Not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("Wrong Password");
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
